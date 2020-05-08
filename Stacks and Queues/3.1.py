4305305474c1255f1e6235d50a065ef1eed892a7
JeetPatel301095 <43115993+JeetPatel301095@users.noreply.github.com>
2020-05-08 18:53:43 -0400

    Removed StackClass

--- Stacks and Queues/3.1.py
+++ Stacks and Queues/3.1.py
@@ -1,5 +1,47 @@
-from StackClass import Stack,Node
 from typing import List
+
+class Node:
+    def __init__(self,val):
+        self.val = val
+        self.next = None
+    
+    def __str__(self) -> str:
+        return str(self.val)
+
+
+class Stack:
+    def __init__(self,top:Node = None):
+        self.top = top
+
+    def isEmpty(self) ->bool:
+        if self.top == None:
+            return True
+        return False
+
+    def push(self,item:int) -> None:
+        temp:Node = Node(item)
+        temp.next = self.top
+        self.top = temp
+
+    def pop(self):
+        if self.isEmpty():
+            raise Exception("Sorry, The Stack is Already Empty")
+        ret = self.top.val
+        self.top = self.top.next
+        return ret.val
+
+    def peek(self):
+        if self.isEmpty():
+            return None
+        return self.top.val
+
+    def __str__(self) -> str:
+        a:str = ""
+        temp:Node = self.top
+        while temp != None:
+            a+= str(temp)+"  "
+            temp=temp.next
+        return a
 
 def threeInOne(s1:Stack,s2:Stack,s3:Stack) ->List[int]:
     ans : List[int] = list()
